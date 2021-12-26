c20c8e2f427de53d9386dbb6260aacfab3f11aa9
林小猪 <52615150+linxiaozhu1@users.noreply.github.com>
2021-12-26 10:52:33 +0800

    Update Q绑查询.js

--- Q绑查询.js
+++ Q绑查询.js
@@ -1,16 +1,14 @@
-// [rule: ?绑定查询]
+// [rule: ?查绑定]
 // [admin: false] 这里默认关闭了、开启true后除管理员账号外、群员不可查
 function main() {
     var userID = GetUserID()
-    console.log(userID)
     var time = (60) //禁言的时间 单位/秒
     var qq = encodeURI(param(1))
-    console.log(qq)
-    if (qq.toString() == "602772889") {
+    if (qq == 602772889) {
         sendText("查我？拉出去毙了") //这里设置机器人QQ、触发限制查询机器人QQ
         return
     }
-    if (qq.toString() == 793364915) {
+    if (qq == 793364915) {
         sendText("不能查群主，禁言一分钟作为惩罚") //这里设置群主QQ、触发限制查询群主、并触发禁言1分钟
         sleep(800)
         GroupBan(userID, time)
@@ -20,7 +18,6 @@ function main() {
         url: "http://ovocc.top/chuanapi/chabangapi2.php?qq=" + qq ,
         "dataType": "json"
     })
-    console.log(data)
     var phone = (data.phone)
     var phonediqu = (data.phonediqu)
     var lol = (data.lol)
